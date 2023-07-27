e6436e98102a2e2ba1df1d58210794931e1d9561
mattia0419 <pironemattia6@gmail.com>
2023-07-27 17:27:34 +0200

    add 2 ex

--- pari e dispari/main.js
+++ pari e dispari/main.js
@@ -3,7 +3,7 @@ let computerNumber;
 const evenButton = document.getElementById("even-button");
 const shotsButton = document.getElementById("shots-button");
 let sum = 0;
-let even = false;
+
 const winLose = document.getElementById("win-lose");
 evenButton.addEventListener("click", function () {
     let userNumberInt = parseInt(userNumber.value);
@@ -11,7 +11,33 @@ evenButton.addEventListener("click", function () {
     console.log(computerNumber);
     sum = userNumberInt + computerNumber;
     console.log(sum);
-    evenShots(sum, even);
+    
+    
+    const even = evenShots(sum);
+    if(even){
+        winLose.innerHTML = "HAI VINTO";
+    }
+    else{
+        winLose.innerHTML = "HAI PERSO";
+    }
+    
+    
+})
+shotsButton.addEventListener("click", function () {
+    let userNumberInt = parseInt(userNumber.value);
+    random();
+    console.log(computerNumber);
+    sum = userNumberInt + computerNumber;
+    console.log(sum);
+    
+    
+    const shots = evenShots(sum);
+    if(shots){
+        winLose.innerHTML = "HAI PERSO";
+    }
+    else{
+        winLose.innerHTML = "HAI VINTO";
+    }
     
     
 })
@@ -39,14 +65,14 @@ function random() {
     document.getElementById("generated-num").innerHTML ="Numero casuale " + computerNumber;
 }
 
-function evenShots(sum, even){
+
+function evenShots(sum){
+    
     if(sum % 2 == 0){
-        even = true;
-        winLose.innerHTML = "HAI VINTO"
+        return true;          
     }
-    else{
-        even = false;
-        winLose.innerHTML = "HAI PERSO"
-    }
-    console.log(even);
-} 
+    
+    
+     return false;
+
+}
